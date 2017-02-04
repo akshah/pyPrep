f4361ceafdbf5eb41256c49dd707d5058082ede8
Anant Shah <akshah@cs.colostate.edu>
2017-02-04 15:11:27 -0700

    Clean-up merge sort

--- sortAlgos/mergeSort.py
+++ sortAlgos/mergeSort.py
@@ -1,9 +1,39 @@
 #!/usr/bin/python
+def merge(leftA, rightA):
+    results=[]
+    li=0
+    ri=0
+    while li<len(leftA) and ri<len(rightA):
+        if leftA[li]<rightA[ri]:
+            results.append(leftA[li])
+            li+=1
+        else:
+            results.append(rightA[ri])
+            ri += 1
+    while li<len(leftA):
+        results.append(leftA[li])
+        li+=1
+    while ri<len(rightA):
+        results.append(rightA[ri])
+        ri+=1
+    return results
 
-def merge_sort(array):
+def mergeSort(array):
+    if len(array)<2:
+        return array
     mid=len(array)//2
-    leftArray=merge_sort(array[:mid])
-    rightArray=merge_sort(array[mid:])
+    leftArray=mergeSort(array[:mid])
+    rightArray=mergeSort(array[mid:])
+    results=merge(leftArray,rightArray)
+
+    return results
+
+
+if __name__=="__main__":
+    arr=[1,3,5,2,5,6,2,3,4,5,6]
+    print(arr)
+    sortedArr=mergeSort(arr)
+    print(sortedArr)
 
     
 
